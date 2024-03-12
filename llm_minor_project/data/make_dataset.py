76e9a19511f6e35a3719998b618f5a4ee9503a77
RBBU <haaglee@gmail.com>
2024-03-12 11:42:04 +0100

    added som data

--- llm_minor_project/data/make_dataset.py
+++ llm_minor_project/data/make_dataset.py
@@ -1,3 +1,23 @@
+import os
+
+def read_raw_data(file_path):
+    with open(file_path, 'r') as file:
+        data = file.read()
+    return data
+
+def process_data(data):
+    # Implement your data processing here
+    # For example, cleaning text, extracting information, etc.
+    return data
+
 if __name__ == '__main__':
-    # Get the data and process it
-    pass
+    raw_data_path = '/home/muna/pdf_file/output.txt'
+    # You might also want to define a path for processed data
+    processed_data_path = '/home/muna/pdf_file/processed_data.txt'
+
+    raw_data = read_raw_data(raw_data_path)
+    processed_data = process_data(raw_data)
+
+    # Optionally save processed data or pass it to another function for further analysis
+    with open(processed_data_path, 'w') as file:
+        file.write(processed_data)
--- mainexample1.py
+++ mainexample1.py
@@ -3,6 +3,9 @@
 import os
 from constants import openai_key
 from langchain.llms import OpenAI
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+
 
 import streamlit as st
 
@@ -16,11 +19,24 @@ input_text=st.text_input('Search the topic you want')
 
 
 
+# Prompt template 
+
+first_input_prompt=PromptTemplate(
+  input_variables=['name'], 
+  template="Tell me about {name}"
+)
 llm=OpenAI(temperature=0.8)
+
+chain=LLMChain(llm=llm, prompt=first_input_prompt, verbose=True, output_key='title')
+
+
+
+
+
 
 
 
 
 
 if input_text:
-  st.write(llm(input_text))
+  st.write(chan.run(input_text))
